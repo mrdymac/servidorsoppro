57c9e73d3007f54442602794729f038e26b8716f
Dyego <sr.dyegomachado@gmail.com>
2019-11-22 12:04:58 -0400

    s

--- routes/empresas.js
+++ routes/empresas.js
@@ -51,7 +51,7 @@ router.post('/ticker/dividendos/save',function(req,res){
    var cod=req.body.codigo;
    var div=req.body.dividendo.replace(',','.');  
    var Empresas = db.Mongoose.model('empresas', db.EmpresasSchema, 'empresas');
-   Empresas.findOne({"tickers.codigo":cod},{tickers}).lean().exec((e,tk)=>{      
+   Empresas.findOne({"tickers.codigo":cod},{tickers:1}).lean().exec((e,tk)=>{      
       Empresas.findOneAndUpdate({"tickers.codigo":cod},{"tickers.codigo.dividendo":div},
        {upsert:true}, function(err, doc){
          if (err)
