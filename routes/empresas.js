3127412ea7bc5896f4941758028556d7270741ba
Dyego <sr.dyegomachado@gmail.com>
2019-11-25 16:44:53 -0400

    asda

--- routes/empresas.js
+++ routes/empresas.js
@@ -193,7 +193,7 @@ router.get('/cotacoes', function(req, res) {
    var Tickers = db.Mongoose.model('tickers', db.TickersSchema, 'tickers');
    Tickers.find({idEmpresa:lastid,codigo:ticker},{cotacoes:1}).lean().exec(
       function (e, docs) { 
-       var lista=docs[0].cotacoes;
+       var lista=docs;
           
        lista.forEach(element => {
            element.data=getDataFormatada(element.data);
