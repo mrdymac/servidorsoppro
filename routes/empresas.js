a7450f19f1e167bda390ce779ba7af9b0ceba9fa
Dyego <sr.dyegomachado@gmail.com>
2019-11-25 12:11:06 -0400

    sada asd

--- routes/empresas.js
+++ routes/empresas.js
@@ -108,7 +108,7 @@ router.post('/ticker/cotacoes/save',function(req,res){
        }  else{
          Empresas.findOneAndUpdate({
             "tickers.codigo":cod,"tickers.cotacoes.data":dat},
-            {$push:{"tickers.$.cotacoes.$":{fechamento:parseFloat(val)}}},
+            {$set:{"tickers.0.cotacoes.0.fechamento":parseFloat(val)}},
             function(err, doc){
               if (err)
               return res.send(500, { error: err });
